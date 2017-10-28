c46e0ee4345bd40dc6d554f66dce70e3ab97944a
Arwa <arwa.it15@gmail.com>
2017-10-28 15:54:16 -0500

    work in progress, technologies

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 'use strict';
+
 require('handlebars');
-const { technologies } = require('./technologies.json');
-let techgrid = require('../partials/technologies.hbs');
-let technologyGrid = techgrid({ technologies });
+let $ = require('jquery');
+
+const { technologies } = require('../technologies.json');
+
+let grid = require('./partials/technologies.hbs');
+
+let technologyGrid = grid({ technologies });
 $('#techGrid').append(technologyGrid);
+// $('#techGrid').on('mouseOver', tooltip('show'));
