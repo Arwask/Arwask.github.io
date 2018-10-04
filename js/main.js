930e4d6338c07b96a03859b95037b0df155f5043
Arwa <arwa.it15@gmail.com>
2018-10-04 11:33:56 -0400

    contact, technologies, projects

--- js/main.js
+++ js/main.js
@@ -7,7 +7,20 @@ const { technologies } = require('../technologies.json');
 
 let grid = require('../partials/technologies.hbs');
 
-console.log('technologies', technologies);
-
 let technologyGrid = grid({ technologies });
 $('#techGrid').append(technologyGrid);
+
+$(function() {
+  var watchScroll = 0;
+  var rightComments = $('.r-event .event-body');
+  var leftComments = $('.l-event .event-body');
+  TweenMax.staggerFrom(rightComments, 1, { x: 100, ease: Bounce.easeOut }, 1);
+  TweenMax.staggerFrom(leftComments, 1, { x: -100, ease: Bounce.easeOut }, 1);
+
+  $(window).on('scroll', function() {
+    var scrollTop = $(window).scrollTop();
+    scrollTop > watchScroll ? $('footer').addClass('footer-up') : $('footer').removeClass('footer-up');
+
+    watchScroll = scrollTop;
+  });
+});
